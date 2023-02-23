2c5555a017aad0ef9a229f9de0cbe8c55ce578db
NicoToP <you@example.com>
2023-02-23 08:37:36 -0500

    Sesion 12

---
+++ Sesion_12/script.js
@@ -0,0 +1,24 @@
+alert("Llamado desde un archivo externo");
+//comentario una linea
+
+/*comentario
+multiple 
+lineas*/
+
+/* 
+Declaración o crear una variable 
+var 
+*/
+var edad;
+
+/*asignación de una variable
+nombre_variable = valor_variable
+*/
+edad=25;
+
+/*
+creación y asignación de valor de una variable
+var nombre_variable = valor_variable;
+*/
+
+var apellido = "perez";
