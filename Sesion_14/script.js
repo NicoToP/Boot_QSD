c9da30936d98ebf06750c09fe43fb17dc8092e94
NicoToP <you@example.com>
2023-02-24 12:04:52 -0500

    Sesion 14

---
+++ Sesion_14/script.js
@@ -0,0 +1,88 @@
+// Estructura condional IF
+/*
+
+var num = 30;
+
+if (num >= 30) {
+    alert("La variable num es mayor o igual que 30");
+}
+
+
+//Operadores matemáticos/aritméticos
+
+
+Mayor (>)
+Menor (<)
+Mayor o igual (>=)
+Menor o igual (<=)
+Igual (==)
+Distinto de (!=)
+
+
+var n1 = 15;
+var n2 = 10;
+//mayor
+if (n1 > n2) {
+    alert("n1 es mayor que n2")
+}
+
+///menor
+if (n1 < n2) {
+    alert(" n1 es menor que n2")
+}
+
+//menor o igual
+if (n1 <= n2) {
+    alert("n1 es menor o igual que n2")
+}
+
+//igual
+if (n1 == n2) {
+    alert("n1 es igual que n2")
+}
+
+//distinto
+if (n1 != n2) {
+    alert("n1 y n2 son distintos")
+}
+
+//Booleano
+var encendido = true;
+
+if (encendido == true) {
+    alert("La lámpara está encendida");
+}
+
+*/
+/* Ejercicio 1
+
+var num1 = 5;
+var num2 = 8;
+
+if (num1 <= num2) {
+    alert(num1 + " NO es mayor que " + num2)
+}
+if (num2 > 0 ) {
+    alert(num2 + " es positivo ")
+}
+if (num1 != 0 ) {
+    alert(num1 + " es negativo o distinto que de cero")
+}
+
+if (num1+1 < num2) {
+    alert("Incrementar en 1 unidad el valor de " + num1 + " no lo hace mayor o igual que " + num2)
+}
+*/
+
+
+/*
+Pedir un número por promt y mostrar un mensaje sólo si el número ingresado es mayor a 10.
+El mensaje puede decir cualquier cosa!*/
+/*
+var nume = prompt("hola, ingresa un número entero (sin puntos ni comas)");
+
+if (nume > 10) {
+    alert("El número es mayor que 10")
+}*/
+
+/
